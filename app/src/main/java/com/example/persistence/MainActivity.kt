3c0aa88a7f9ffc14fbe3dd8818f6af9b3196e6e0
Sebastian Gielen Pereira <132985652+sebagipe1@users.noreply.github.com>
2025-06-15 12:16:43 +0200

    Removed Settings from MainActivity

--- app/src/main/java/com/example/persistence/MainActivity.kt
+++ app/src/main/java/com/example/persistence/MainActivity.kt
@@ -23,7 +23,6 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.persistence.ui.Screens.SettingsScreen
 import com.example.persistence.ui.Screens.ToDoScreen
 import com.example.persistence.ui.theme.PersistenceTheme
 import kotlinx.serialization.Serializable
@@ -31,80 +30,26 @@ import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
 
-    val settingsViewModel by viewModels<SettingsViewModel>()
-
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Settings
-            val isAlwaysOnScreen by settingsViewModel.dataStore.alwaysOnScreenFlow.collectAsStateWithLifecycle(initialValue = false)
-            if (isAlwaysOnScreen) window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-            else window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-
             PersistenceTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = ToDoScreen) {
-                    composable<ToDoScreen> {
-                        Scaffold(
-                            modifier = Modifier,
-                            topBar = {
-                                CenterAlignedTopAppBar(
-                                    title = { Text("To-Do") },
-                                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                                        containerColor = MaterialTheme.colorScheme.primaryContainer
-                                    ),
-                                    actions = {
-                                        IconButton(onClick = { navController.navigate(SettingsScreen) }) {
-                                            Icon(
-                                                imageVector = Icons.Filled.Settings,
-                                                contentDescription = "Settings Button"
-                                            )
-                                        }
-                                    }
-                                )
-                            },
-                            content = {
-                                ToDoScreen(modifier = Modifier.padding(it))
-                            }
+                Scaffold(
+                    modifier = Modifier,
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = { Text("To-Do") },
+                            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer
+                            )
                         )
+                    },
+                    content = {
+                        ToDoScreen(modifier = Modifier.padding(it))
                     }
-                    composable<SettingsScreen> {
-                        Scaffold(
-                            modifier = Modifier,
-                            topBar = {
-                                CenterAlignedTopAppBar(
-                                    navigationIcon = {
-                                        IconButton(onClick = { navController.popBackStack() }) {
-                                            Icon(
-                                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                                contentDescription = "Arrow Back Button"
-                                            )
-                                        }
-                                    },
-                                    title = { Text("Settings") },
-                                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                                        containerColor = MaterialTheme.colorScheme.primaryContainer
-                                    ),
-                                )
-                            },
-                            content = {
-                                SettingsScreen(
-                                    viewModel = settingsViewModel,
-                                    modifier = Modifier.padding(it),
-                                    isAlwaysOnScreen = isAlwaysOnScreen,
-                                )
-                            }
-                        )
-                    }
-                }
+                )
             }
         }
     }
 }
-
-@Serializable
-object ToDoScreen
-
-@Serializable
-object SettingsScreen
